c43f7efa9fc6c74903a804473759bd325a0dbeda
Luke <lavedon@gmail.com>
2023-04-05 00:01:41 -0400

    datatables select extension works as well as possible. Unless there is more to shift select

--- src/app/my-select-extension/my-select-extension.component.ts
+++ src/app/my-select-extension/my-select-extension.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, Component, OnInit, AfterViewInit } from '@angular/core';
 import * as $ from 'jquery';
 import { mockData } from '../mock-data';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'my-select-extension',
   templateUrl: './my-select-extension.component.html',
   styleUrls: ['./my-select-extension.component.css']
 })
-export class MySelectExtensionComponent implements OnInit {
+export class MySelectExtensionComponent implements OnInit, AfterViewInit {
+  @ViewChild(DataTableDirective, { static: false })
+  datatableElement: DataTableDirective = {} as DataTableDirective;
 
   mockData = mockData;
   dt2ndOptions: any = {};
 
   ngOnInit(): void {
     this.dt2ndOptions = {
-      pagingType: 'full_numbers',
-      lengthMenu: [5, 3],
-      processing: true,
-      select: true
-    };
+      select: 'os'
+    }
   }
+
+  ngAfterViewInit(): void {
+    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.on('user-select', (e, dt, type, cell, originalEvent) => {
+          let rowData = (dtInstance.row(cell.node()).data() as any[])[3];
+          if (rowData === 'false') {
+            e.preventDefault();
+          }
+        });
+      });
+    }
 }
